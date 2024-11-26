921da4412d2244afa7a77bb6afeb58752ae69242
𝚂𝙰𝚂𝙺𝚈𝙾 <tomasdada.server.op@gmail.com>
2024-11-26 16:19:59 +0000

    Update part4.cs

--- delegates/part4.cs
+++ delegates/part4.cs
@@ -1,18 +1,20 @@
 using System;
-using System.Collections.Generic;
 
-class Program
+namespace Delegates
 {
-    static bool IsEven(int number)
+    class Program
     {
-        return number % 2 == 0;
-    }
-
-    static void Main()
-    {
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        Predicate<int> isEven = IsEven;
-        List<int> evenNumbers = numbers.FindAll(isEven);
-        Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Predicate<int> isEven = IsEven;
+            List<int> evenNumbers = numbers.FindAll(isEven);
+            Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+        }
     }
 }
