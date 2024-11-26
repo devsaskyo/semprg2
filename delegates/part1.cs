e0d8fb4df1c604d4169903127f5bb951d960c93d
𝚂𝙰𝚂𝙺𝚈𝙾 <tomasdada.server.op@gmail.com>
2024-11-26 15:56:37 +0000

    Create part1.cs

---
+++ delegates/part1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+delegate int DoMath(int x, int y);
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DoMath output1 = Add;
+        DoMath output2 = Subtract;
+        Console.WriteLine($"output1: {output1(1,1)}\noutput2: {output2(1,1)}");
+    }
+    
+    static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    
+    static int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+}
