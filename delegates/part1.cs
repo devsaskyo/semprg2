59fcb3dbed6b65c55b5fe0ca2d0ef12d30650d31
𝚂𝙰𝚂𝙺𝚈𝙾 <tomasdada.server.op@gmail.com>
2024-11-26 16:17:57 +0000

    Update part1.cs

--- delegates/part1.cs
+++ delegates/part1.cs
@@ -4,22 +4,25 @@ using System.Linq;
 
 delegate int DoMath(int x, int y);
 
-public class Program
+namespace Delegates
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        DoMath output1 = Add;
-        DoMath output2 = Subtract;
-        Console.WriteLine($"output1: {output1(1,1)}\noutput2: {output2(1,1)}");
-    }
-    
-    static int Add(int x, int y)
-    {
-        return x + y;
-    }
-    
-    static int Subtract(int x, int y)
-    {
-        return x - y;
+        public static void Main(string[] args)
+        {
+            DoMath output1 = Add;
+            DoMath output2 = Subtract;
+            Console.WriteLine($"output1: {output1(1,1)}\noutput2: {output2(1,1)}");
+        }
+        
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
     }
 }
