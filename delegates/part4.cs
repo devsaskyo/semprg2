692bf7869fdc641fc4bccc7752078a919ee4f17f
𝚂𝙰𝚂𝙺𝚈𝙾 <tomasdada.server.op@gmail.com>
2024-11-29 08:04:23 +0100

    Update part4.cs

--- delegates/part4.cs
+++ delegates/part4.cs
@@ -10,7 +10,7 @@ namespace Delegates
             return number % 2 == 0;
         }
     
-        static void Main()
+        public static void Main(string[] args)
         {
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Predicate<int> isEven = IsEven;
